35101952333bef0e125c19680b109ce2b6012a48
supopur <matylda.smyk.08@gmail.com>
2022-12-24 12:49:49 +0100

    Commiting so if the new token expiration system breaks the whole code i can restore

--- api.py
+++ api.py
@@ -2,11 +2,12 @@
 #Modified by https://github.com/supopur/
 #I got permision. You can search for my issue on github on his repo.
 
-import redis, yaml, os, json, pathlib
+import redis, yaml, os, json, pathlib, secrets, hashlib, threading, datetime
 from flask import Flask
 from flask import Flask, request
 from flask_restful import Resource, Api
 from redis.commands.json.path import Path
+
 
 #Get password from the yml file
 def get_password():
@@ -28,13 +29,26 @@ else:
 app = Flask(__name__)
 api = Api(app)
 
-
+#Check if the value is a valid json format
 def check_json(data):
     try:
         json.loads(data)
     except ValueError:
         return False
     return True
+
+#Make a client secret used for authenticating users
+def generate_token():
+    #Generates a random, secure token.
+    return secrets.token_hex(50)
+
+#Create a redis string key that expires after certain time in days
+def create_expire_key(key, value, days):
+    # Set the key with the value
+    r.set(key, value)
+    ttl_in_seconds = days * 24 * 60 * 60
+    # Set the key's TTL to 1 week
+    r.expire(key, ttl_in_seconds)
 
 class Save(Resource):
     def post(self, player_name):
@@ -57,6 +71,18 @@ class Load(Resource):
             return {"error": f"Key '{key}' not found"}, 404
         return {"key": key, "value": value}
 
+class GenSecret(Resource):
+    def get(self, player):
+        if r.exists(f"{player}_secret"):
+            return "Error token exists", 409
+        elif not r.exists(f"{player}_secret"):
+            secret = generate_token()
+            create_expire_key(f"{player}_secret", secret, 7)
+            return secret, 200
+        else:
+            return "Internal Server Error", 500
+
+api.add_resource(GenSecret, "/gensecret/<string:player>")
 api.add_resource(Save, "/save/<string:player_name>")
 api.add_resource(Load, "/load/<string:key>")
 
