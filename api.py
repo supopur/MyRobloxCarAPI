697f136e41b0ab330aaa7b090a05b91d3c23a82e
supopur <matylda.smyk.08@gmail.com>
2022-12-24 11:03:29 +0100

    Rewrote half of the code to better support json

--- api.py
+++ api.py
@@ -1,11 +1,12 @@
-import redis, yaml, os
-from pathlib import Path
+import redis, yaml, os, json, pathlib
 from flask import Flask
-
+from flask import Flask, request
+from flask_restful import Resource, Api
+from redis.commands.json.path import Path
 
 #Get password from the yml file
 def get_password():
-    full_file_path = Path(__file__).parent.joinpath('credentials.yml')
+    full_file_path = pathlib.Path(__file__).parent.joinpath('credentials.yml')
     with open(full_file_path) as settings:
         settings_data = yaml.load(settings, Loader=yaml.Loader)
     return settings_data
@@ -14,44 +15,46 @@ creds = get_password()
 mode = creds["mode"]
 # redis
 if mode == "userpass":
-    redis_cache = redis.Redis(host=creds["host"], port=creds["port"], db=0, password=creds["password"], username=creds["user"])
+    r = redis.Redis(host=creds["host"], port=creds["port"], db=0, password=creds["password"], username=creds["user"])
 elif mode == "nopass":
-    redis_cache = redis.Redis(host=creds["host"], port=creds["port"], db=0, user=creds["user"])
+    r = redis.Redis(host=creds["host"], port=creds["port"], db=0, user=creds["user"])
 else:
     print("No valid login method specified so now im not doing it!")
     os.exit()
-
-# flask app
 app = Flask(__name__)
+api = Api(app)
 
 
-# set
-@app.route('/set/<string:key>/<string:value>')
-def set(key, value):
-	if redis_cache.exists(key):
-		return f"{key} is already exists, please use `update` route to change the value!"
-	else:
-		redis_cache.set(key, value)
-		return "OK"
+def check_json(data):
+    try:
+        json.loads(data)
+    except ValueError:
+        return False
+    return True
 
-# update
-@app.route('/update/<string:key>/<string:value>')
-def update(key, value):
-	if redis_cache.exists(key):
-		redis_cache.set(key, value)
-		return "OK"
-	else:
-		return f"{key} is not exists"
+class Save(Resource):
+    def post(self, player_name):
+        data = request.data
+        if not check_json(data):
+            return {"error": "Invalid JSON format"}, 400
+        data = json.loads(data)
+        print(data)
 
-# get
-@app.route('/get/<string:key>')
-def get(key):
-    if redis_cache.exists(key):
-        print(redis_cache.json().get(key))
-        return redis_cache.json().get(key)
-    else:
-        return f"{key} is not exists"
+        r.json().set(player_name, Path.root_path(), data["value"])
+        return {"status": "success"}
 
+
+class Load(Resource):
+    def get(self, key):
+
+        value = r.json().get(key)
+        print(value)
+        if value is None:
+            return {"error": f"Key '{key}' not found"}, 404
+        return {"key": key, "value": value}
+
+api.add_resource(Save, "/save/<string:player_name>")
+api.add_resource(Load, "/load/<string:key>")
 
 if __name__ == "__main__":
-	app.run("0.0.0.0", port="5000", debug=True)
+    app.run("0.0.0.0", port="5000", debug=True)
